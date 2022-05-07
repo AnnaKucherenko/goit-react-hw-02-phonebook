6a78c3fd87cdfa25c73542281c2e307555cc9136
AnnaKucherenko <annakucherenko31@gmail.com>
2022-05-07 21:19:01 +0300

    Сборка проекту

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+  
+class App extends Component {
+  state = {
+    contacts: [],
+    name: ''
+  };
+
+  
+  render() {
+    
+    
+    return (
+      <div>
+        hello
+      </div>
+    ); 
+  }
+};
+
+export default  App;
